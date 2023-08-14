eeeefa663eec11c51ea6485f337bd76eb501e597
Pascal Vogel <pascal-vogel@gmx.de>
2023-08-14 14:53:09 +0200

    Wiederholungsschleife eingefügt

--- HalloGitHub/Program.cs
+++ HalloGitHub/Program.cs
@@ -1,14 +1,24 @@
-﻿using System;
+﻿string s = "";
 
-namespace MyApp // Note: actual namespace depends on the project name.
+while (s != "n")
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+    //Abfrage
+    Console.WriteLine("Geben sie eine Zahl zwischen 0-10 ein");
 
-            Console.ReadKey();  
-        }
+    int a = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine(a);
+
+    //Schleife
+    while (a < 0 || a > 10)
+    {
+        Console.WriteLine("Sie müssen eine Zahl zwischen 0-10 eingeben");
+        a = Convert.ToInt32(Console.ReadLine());
     }
+
+    //Antwort
+    Console.WriteLine(String.Format("Sie haben es geschafft {0} ist eine zahl zwischen 0-10", a));
+
+    Console.WriteLine("Möchten sie nochmal eine Zahl eingeben y/n");
+    s = Console.ReadLine();
 }
